00cb7e6956fc9bcab2b593971d0568b686f083eb
auterrykp <40428914+auterrykp@users.noreply.github.com>
2025-05-03 12:40:02 +0800

    Update font-master.js

--- font-master.js
+++ font-master.js
@@ -13,7 +13,7 @@
 // ==/UserScript==
 
 
-const allowedDomains = ['scmp.com','mingpao.com'];
+const allowedDomains = ['jobsdb.com','mingpao.com'];
 
   if (allowedDomains.some(domain => window.location.hostname.endsWith(domain))) {
     document.documentElement.classList.add('--serif-font');
